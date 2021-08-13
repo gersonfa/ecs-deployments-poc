8c85e96b29019306c29213864c8d6861963afb0c
Gerson Flores Acuña <gersosvald@MacBook-Pro.local>
2021-08-12 21:01:51 -0500

    create workflow

--- service.tf
+++ service.tf
@@ -1,5 +1,5 @@
 resource "aws_ecs_service" "this" {
-  name            = "staging"
+  name            = "generator"
   cluster         = aws_ecs_cluster.this.id
   task_definition = aws_ecs_task_definition.service.arn
   desired_count   = 1
